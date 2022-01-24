3a53bc489e048a7866e3ebeb4e0d7e32e8d76b6c
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-24 18:21:43 +0000

    new syntax

--- dayonetasks.py
+++ dayonetasks.py
@@ -11,7 +11,7 @@ print("Hello World")
 
 # Write a program that determines the name of a shape from its number of sides. Read the number of sides from the user and then report the appropriate name as part of a meaningful message. Your program should support shapes with anywhere from 3 up to (and including) 10 sides. If a number of sides outside of this range is entered then your program should display an appropriate error message.
 
-shapes = int(input("enter the amount of sides your shape has"))
+shapes = int(input("enter the amount of sides for your shape"))
 
 shapekernel = ["triangle", "square", "pentagon", "hexagon", "heptagon", "octagon", "enneagon", "decagon"]
 
