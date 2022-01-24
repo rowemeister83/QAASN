b1a2a3534a43f1bb576a2908e632fb6af5e59073
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-24 18:00:22 +0000

    did vowel task in pycharm -wont push to branch

--- dayonetasks.py
+++ dayonetasks.py
@@ -1,11 +1,12 @@
 print("Hello World")
 
-# Write a program that reads an integer from the user. Then your program should display a message indicating whether the integer is even or odd.
+# In this exercise you will create a program that reads a letter of the alphabet from the user. If the user enters a,e,i,o or u then your program should display a message indicating that the entered letter is a vowel. If the user enters y then your program should display a message indicating that sometimes y is a vowel, and sometimes y is a consonant. Otherwise your program should display a message indicating that the letter is a consonant.
 
 
 # Write a program that reads an integer from the user. Then your program should display a message indicating whether the integer is even or odd.
 
 
+# Write a program that reads an integer from the user. Then your program should display a message indicating whether the integer is even or odd.
 
 
 # Write a program that determines the name of a shape from its number of sides. Read the number of sides from the user and then report the appropriate name as part of a meaningful message. Your program should support shapes with anywhere from 3 up to (and including) 10 sides. If a number of sides outside of this range is entered then your program should display an appropriate error message.
