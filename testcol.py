2f2390bda1f768186c5117576a59d585f6d6a267
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-24 14:55:51 +0000

    Added the Doyens of Sci-Fi

--- testcol.py
+++ testcol.py
@@ -1,2 +1,7 @@
 books = {"The Handmaid's Tale":"Margaret Atwood", "The Hobbit":"J.R.R. Tolkien", "Charlie and the Chocolate Factory":"Roald Dahl"}
 print(books["The Handmaid's Tale"])
+
+books.update({"The Left Hand of Darkness": "Ursula K Le Guin", "Dune": "Frank Herbert"})
+print(books)
+
+print(books["The Left Hand of Darkness"])
