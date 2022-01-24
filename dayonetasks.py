dd88eee6aa9b63fd6d8abe513ae14dbbcd7478f3
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-24 18:29:30 +0000

    odds and evens and alphablocks

--- dayonetasks.py
+++ dayonetasks.py
@@ -2,16 +2,24 @@ print("Hello World")
 
 # In this exercise you will create a program that reads a letter of the alphabet from the user. If the user enters a,e,i,o or u then your program should display a message indicating that the entered letter is a vowel. If the user enters y then your program should display a message indicating that sometimes y is a vowel, and sometimes y is a consonant. Otherwise your program should display a message indicating that the letter is a consonant.
 
+letter = input("Pick any letter: ")
+
+if letter in('a', 'e', 'i', 'o', 'u'):
+	print("%s is a Vowel." % letter)
+elif letter == 'y':
+	print("Sometimes \'y\' is a vowel, sometimes \'y\' is a consonant.")
+else:
+	print("%s is a Consonant." % letter) 
+
+# Write a program that reads an integer from the user. Then your program should display a message indicating whether the integer is even or odd.
+
 num = int(input("Pick a number, any number: "))
  
 if (num % 2) == 0:
    print("{0} is Even".format(num))
-   
+
 else:
    print("{0} is Odd".format(num))
-
-# Write a program that reads an integer from the user. Then your program should display a message indicating whether the integer is even or odd.
-
 
 # Write a program that reads an integer from the user. Then your program should display a message indicating whether the integer is even or odd.
 
