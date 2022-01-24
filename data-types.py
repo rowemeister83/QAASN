e29c207c03c9f2fce9cdefcab89cddd7b205e76f
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-24 12:50:39 +0000

    dog's in the bag

--- data-types.py
+++ data-types.py
@@ -1,9 +1,9 @@
 rogue1 = "Andrew"
-rogue2 = "is Obi Wan"
+rogue2 = "is Obi Wan,"
 rogue3 = "R2D2"
 bestpilot = "Redleader"
 
-print(rogue1,rogue2, "but the best pilot is", bestpilot, "and", rogue3, "is his overlord")
+print(rogue1,rogue2,"but the best pilot is", bestpilot, "and", rogue3, "is his overly-cautious droid")
 
 name = "Pep Guardogiola"
 age = 3
