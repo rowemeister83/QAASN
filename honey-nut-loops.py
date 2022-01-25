3c7ecc81e14b66fbeb06c5eac8edc0d908a1334a
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-25 10:11:10 +0000

    There is no spoon

--- honey-nut-loops.py
+++ honey-nut-loops.py
@@ -18,3 +18,13 @@ while countdown >40:
 
 for fish in ["cat", "dog", "lemur", "brazillian river snake", "Andelusian mountain llama"]:
     print(fish)
+
+dictvar1 = {"cow":"moo" , "sheep":"baa" , "chicken":"cluck" , "dog":"bowow" }
+
+print(dictvar1.keys())
+
+for i in range(5):
+    print("i love pies")
+
+for i in "spoooooon":
+    print(i)
