867d513e24e45058baa336123caf3159c928d872
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-25 11:03:45 +0000

    started w3 exercises

---
+++ w3exercises.py
@@ -0,0 +1,23 @@
+#exercise one
+
+for i in range(1500,2701):
+    if i%7==0 and i%5==0:
+        print(i)
+
+#exercise two
+
+celsius = int(input("Enter celsius temp:"))
+fahrenheit = (celsius * 1.8) + 32
+print('%.0f celsius is %0.1f fahrenheit' %(celsius, fahrenheit))
+fahrenheit = int(input("Enter fahrenheit temp:"))
+celsius = (fahrenheit - 32) / 1.8
+print('%.0f fahrenheit is %0.1f Celsius' %(fahrenheit, celsius))
+
+#exercise three
+
+
+
+
+#exercise four
+
+
