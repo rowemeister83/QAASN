1e6fcca530ca253782d2ffed1dc18dd9ec2f0f6e
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-25 10:23:27 +0000

    qa comm done

--- honey-nut-loops.py
+++ honey-nut-loops.py
@@ -28,3 +28,27 @@ for i in range(5):
 
 for i in "spoooooon":
     print(i)
+
+my_fruit = ["Apple", "Banana", "Orange"]
+
+for fruit in my_fruit:
+    print(fruit.upper())
+
+for word in "Hello world".split():
+    print(word)
+
+for key in {"key": "value"}:
+    print(key)
+
+for value in {"key": "value"}.values():
+    print(value)
+
+for key, value in {"key": "value"}.items():
+    print(key, value)
+
+for i in range(3):
+    for j in range(4):
+        print(i, "x", j, "=", i * j)
+
+for i in range(10, 21, 2):
+    print(i)
