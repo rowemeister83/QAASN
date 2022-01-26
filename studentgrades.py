9dc3bc64448477b70f3b7d7b7f25ce0297976479
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-26 15:08:02 +0000

    calced out

--- studentgrades.py
+++ studentgrades.py
@@ -1,18 +1,21 @@
-class Student:
+class Students:
 
-     def __init__(self, Name, TSCORE, age):
-        self.Name = Name
-        self.TSCORE = TSCORE
-        self.age = age 
+    def __init__(self, age, classname,name="student"):
+        self.age = age
+        self.classname = classname
+        self.name = name
 
-John = Student("John", "80", "21")
-Jane = Student("Jane", "80", "22")
-Terry = Student("Terry", "90", "21")
- 
+    def calcavgscore(self,score1:int ,score2:int ,score3:int):
+        avgscore = (score1 + score2 + score3)/3
+        return avgscore
 
+FreakMama = Students(55, "Biden",[5,5,5])
+EvilPapa = Students(66, "Russia")
+SecretSquirrel = Students(103, "Ukraine")
 
-print("Average scores of Students:", TSCORE) 
-
+print( "FreakMama: ", FreakMama.calcavgscore(56, 5, 56))
+print( "EvilPapa: ", EvilPapa.calcavgscore(105, 5, 77))
+print( "SecretSquirrel: ", SecretSquirrel.calcavgscore(20, 10, 70))
 
 
 
