596a6a129779d3ce74e928462a256082ba224b55
rowemeister83 <arworkrelatedmail@gmail.com>
2022-01-26 14:28:48 +0000

    redo calculation for scores - syntax broke

---
+++ studentgrades.py
@@ -0,0 +1,25 @@
+from tokenize import Name
+
+
+class Student:
+
+     def __init__(self, Name, TSCORE, age):
+        self.Name = Name
+        self.TSCORE = TSCORE
+        self.age = age 
+
+John = Student("John", "80", "21")
+Jane = Student("Jane", "80", "22")
+Terry = Student("Terry", "90", "21")
+ 
+def people(avgTSCORE):
+        if Name.total==3:
+            avgTSCORE=0
+
+print("Average scores of Students:", avgTSCORE)
+
+
+
+
+
+
